65d2f08f6b18d80adaea1cc6cff8fa062bf987d0
khatrimann <khatri.mann@gmail.com>
2017-11-05 01:32:05 +0530

    added firebase

--- app/src/main/java/com/example/vaibhav/hypershopping/MainActivity.java
+++ app/src/main/java/com/example/vaibhav/hypershopping/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.vaibhav.hypershopping;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hypertrack.lib.HyperTrack;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HyperTrack.initialize(this, "pk_test_96219ff5ed8a84e70b538a99499b748fe9a5c888");
     }
+
 }
