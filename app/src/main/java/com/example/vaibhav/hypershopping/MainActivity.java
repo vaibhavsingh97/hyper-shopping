140ce447cb4dfbf1890c337183c71de63a6f8b6c
khatrimann <khatri.mann@gmail.com>
2017-11-05 14:59:06 +0530

    SDK commits

--- app/src/main/java/com/example/vaibhav/hypershopping/MainActivity.java
+++ app/src/main/java/com/example/vaibhav/hypershopping/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.vaibhav.hypershopping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.hypertrack.lib.HyperTrack;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        HyperTrack.initialize(this, "pk_test_96219ff5ed8a84e70b538a99499b748fe9a5c888");
+        HyperTrack.initialize(this, "pk_3f3c8ec252a417a10707f6457bb7018601f89036");
+        HyperTrack.requestPermissions(this);
+        HyperTrack.requestLocationServices(this);
+
+        Button b = (Button) findViewById(R.id.button3);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
